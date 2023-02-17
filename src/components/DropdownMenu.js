bb70b35febb65e34aa7005899fa2e3b6aed279af
sml <smlrodrigues.dev@gmail.com>
2023-02-17 11:48:46 -0300

    Implement a dropdown menu on the image

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,9 +1,62 @@
-function DropdownMenu() {
+import '../assets/styles/DropdownMenu.css';
+
+
+function DropdownMenu(props) {
+  const {position, hitPosition, setShowMenu} = props;
+
+  const answers = [
+    {
+      startX: 1009,
+      endX: 1072,
+      startY: 436,
+      endY: 539,
+    },
+    {
+      startX: 1009,
+      endX: 1072,
+      startY: 436,
+      endY: 539,
+    },
+    {
+      startX: 1009,
+      endX: 1072,
+      startY: 436,
+      endY: 539,
+    },
+  ];
+
+  const getAnswerRanger = (startX, endX, startY, endY) => {
+    const x = [];
+    const y = [];
+    for(let i = startX; i <= endX; i++) {
+      x.push(i);
+    }
+
+    for(let i = startY; i <= endY; i++) {
+      y.push(i);
+    }
+
+    return {x, y}
+  }
+
+  const checkAnswer = (position, index) => {
+    const answersRanger = getAnswerRanger(answers[index].startX, answers[index].endX, answers[index].startY, answers[index].endY);
+    const isHitX = answersRanger.x.some((element) => element === position.x);
+    const isHitY = answersRanger.y.some((element) => element === position.y);
+    if (isHitX && isHitY) {
+      console.log('hit');
+    } else {
+      console.log('miss');
+    }
+    setShowMenu(false);
+  }
+
   return (
-    <div id="DropdownMenu">
-      <div>Option</div>
-      <div>Option</div>
-      <div>Option</div>
+    <div id="DropdownMenu"
+          style={{left: `${position.x}px`, top: `${position.y}px` }}>
+      <div onClick={() => checkAnswer(hitPosition, 0)}>Option</div>
+      <div onClick={() => checkAnswer(hitPosition, 1)}>Option</div>
+      <div onClick={() => checkAnswer(hitPosition, 2)}>Option</div>
     </div>
   );
 }
--- src/components/GameImage.js
+++ src/components/GameImage.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import imgGame from '../assets/images/the-loc-nar.jpg';
 import '../assets/styles/GameImage.css';
+import DropdownMenu from './DropdownMenu';
 
 function GameImage() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [imagePosition, setImagePosition] = useState(null);
+  const [hitPosition, setHitPosition] = useState(null);
+
+  const handlePositions = (event) => {
+    if (showMenu) {
+      setShowMenu(false);
+      return;
+    }
+    const offsetX = event.nativeEvent.offsetX;
+    const offsetY = event.nativeEvent.offsetY;
+    const naturalWidth = event.target.naturalWidth;
+    const naturalHeight = event.target.naturalHeight;
+    const clientWidth = event.target.clientWidth;
+    const clientHeight = event.target.clientHeight;
+    const naturalX = Math.round((naturalWidth * offsetX) / clientWidth);
+    const naturalY = Math.round((naturalHeight * offsetY) / clientHeight);
+    setImagePosition({x: offsetX, y: offsetY});
+    setHitPosition({x: naturalX, y: naturalY});
+    setShowMenu(true)
+  }
+
   return (
-    <div>
-      <img src={imgGame}/>
+    <div id='GameImage'>
+      <img src={imgGame} draggable='false' onClick={(event) => handlePositions(event)}/>
+      {showMenu ? <DropdownMenu setShowMenu={setShowMenu} position={imagePosition} hitPosition={hitPosition}/> : null}
     </div>
   )
 }
